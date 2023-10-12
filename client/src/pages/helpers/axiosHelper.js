b735fa2354fea9e37ec5cee43939c744d3675279
prashantbismani <prashant.bismani@7-11.com>
2023-10-12 17:25:28 +0530

    API Base path update to deployed instance

--- client/src/pages/helpers/axiosHelper.js
+++ client/src/pages/helpers/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8080';
+const BASE_URL = 'https://customer-feedback-api.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
